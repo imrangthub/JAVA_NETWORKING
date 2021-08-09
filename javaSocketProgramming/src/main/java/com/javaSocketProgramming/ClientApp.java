f27802bef9b1301d27279d1cd50629cdd2c54b8d
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-08-09 17:21:01 +0600

    add proper file

--- javaSocketProgramming/src/main/java/com/javaSocketProgramming/ClientApp.java
+++ javaSocketProgramming/src/main/java/com/javaSocketProgramming/ClientApp.java
@@ -10,24 +10,22 @@ public class ClientApp {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
 
-        System.out.println("Start Socker Programming Client !");
-        
-        Socket sock = new Socket("127.0.0.1", 9999);
-        System.out.println("Connected to server !");
-        
-        InputStream in = sock.getInputStream();
-        OutputStream out = sock.getOutputStream();
-       
-        
-        out.write("Hello from client".getBytes());
-        
-        byte []response= new byte[1024];
-        in.read(response);
-        
-        System.out.println("Receive from Sever: "+ new String(response).trim());
-        
-        sock.close();
+		System.out.println("Start Socker Programming Client !");
 
+		Socket sock = new Socket("127.0.0.1", 9999);
+		System.out.println("Connected to server !");
+
+		InputStream in = sock.getInputStream();
+		OutputStream out = sock.getOutputStream();
+
+		out.write("Hello from client".getBytes());
+
+		byte[] response = new byte[1024];
+		in.read(response);
+
+		System.out.println("Receive from Sever: " + new String(response).trim());
+
+		sock.close();
 	}
 
 }
--- javaSocketProgramming/src/main/java/com/javaSocketProgramming/ServerApp.java
+++ javaSocketProgramming/src/main/java/com/javaSocketProgramming/ServerApp.java
@@ -10,27 +10,25 @@ public class ServerApp {
 
 	public static void main(String[] args) throws IOException {
 
-        System.out.println("Start Socker Programming Server !");
-        System.out.println("Wating from client !");
-        ServerSocket serSocket = new ServerSocket(9999);
-        
-        Socket sock = serSocket.accept();
-        System.out.println("Client connected !");
-        
-        InputStream in = sock.getInputStream();
-        OutputStream out = sock.getOutputStream();
-        byte buffer[] = new byte[1024];
-        
-        in.read(buffer);
-        System.out.println("Received from Client: "+new String(buffer).trim());
-        
-        
-        out.write("Hellow from Server".getBytes());
- 
-        
-        sock.close();
-        serSocket.close();
+		System.out.println("Start Socker Programming Server !");
+		System.out.println("Wating from client !");
+		ServerSocket serSocket = new ServerSocket(9999);
 
+		Socket sock = serSocket.accept();
+		System.out.println("Client connected !");
+
+		InputStream in = sock.getInputStream();
+		OutputStream out = sock.getOutputStream();
+		byte buffer[] = new byte[1024];
+
+		
+		in.read(buffer);
+		System.out.println("Received from Client: " + new String(buffer).trim());
+
+		out.write("Hellow from Server".getBytes());
+
+		sock.close();
+		serSocket.close();
 	}
 
 }
