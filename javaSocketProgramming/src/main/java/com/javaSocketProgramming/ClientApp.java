1a5f651edf2948334a255e573f702f3e38a0954d
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-08-09 17:17:36 +0600

    Init project with socket example

---
+++ javaSocketProgramming/src/main/java/com/javaSocketProgramming/ClientApp.java
@@ -0,0 +1,33 @@
+package com.javaSocketProgramming;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientApp {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+
+        System.out.println("Start Socker Programming Client !");
+        
+        Socket sock = new Socket("127.0.0.1", 9999);
+        System.out.println("Connected to server !");
+        
+        InputStream in = sock.getInputStream();
+        OutputStream out = sock.getOutputStream();
+       
+        
+        out.write("Hello from client".getBytes());
+        
+        byte []response= new byte[1024];
+        in.read(response);
+        
+        System.out.println("Receive from Sever: "+ new String(response).trim());
+        
+        sock.close();
+
+	}
+
+}
---
+++ javaSocketProgramming/src/main/java/com/javaSocketProgramming/ServerApp.java
@@ -0,0 +1,36 @@
+package com.javaSocketProgramming;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerApp {
+
+	public static void main(String[] args) throws IOException {
+
+        System.out.println("Start Socker Programming Server !");
+        System.out.println("Wating from client !");
+        ServerSocket serSocket = new ServerSocket(9999);
+        
+        Socket sock = serSocket.accept();
+        System.out.println("Client connected !");
+        
+        InputStream in = sock.getInputStream();
+        OutputStream out = sock.getOutputStream();
+        byte buffer[] = new byte[1024];
+        
+        in.read(buffer);
+        System.out.println("Received from Client: "+new String(buffer).trim());
+        
+        
+        out.write("Hellow from Server".getBytes());
+ 
+        
+        sock.close();
+        serSocket.close();
+
+	}
+
+}
